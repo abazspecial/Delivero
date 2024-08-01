21cf49ced0871bad2a9023cb8a68d7e61aeb4942
abazspecial <abazspecial@gmail.com>
2024-08-01 18:05:54 -0500

    Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,93 @@
-import Image from "next/image";
+import Link from "next/link";
+import {FaShieldAlt} from "react-icons/fa"
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <nav className="flex justify-between px-[7%] items-center">
+        <h1>Delivro</h1>
+        <ul className="flex gap-6">
+          <li>
+            <Link href="/">Features</Link>
+          </li>
+          <li>
+            <Link href="/">Price Plans</Link>
+          </li>
+          <li>
+            <Link href="/">About Us</Link>
+          </li>
+        </ul>
+        <button className="contact"> Contact Us</button>
+      </nav>
+      <section className="welcome h-[570px] w-[100%] bg-[#f2f2ff] p-[7%] flex items-center">
+        <div className="w-[60%] flex flex-col">
+          <h1 className="text-[45px] font-semibold">Smart Shipping that grows your Business</h1>
+          <p className="my-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum in voluptates ipsum aliquam deleniti, aperiam eaque eum maxime accusamus nemo doloremque architecto pariatur veniam quas vel qui dolore dolorum. Recusandae?</p>
+          <div className="mt-3 h-auto w-[100%]">
+            <button className="welcome-btn welcome-btn1">Start Free Trial</button>
+            <button className="welcome-btn">Learn more</button>
+          </div>
         </div>
-      </div>
+      </section>
+      <section className="h-[560px] w-[100%] flex px-[7%] py-14 justify-between">
+        <div className="w-[48%]">
+          <h1 className="text-[30px] font-bold">Fully integrated with 10 Buy Shipping tools</h1>
+          <p>Automation tools that simplify your shipping</p>
+          <img src="/favicon.ico" alt="Icon" />
+        </div>
+        <div className="w-[48%] p-6">
+          <div className="fully-div">
+            <FaShieldAlt className="shield"/>
+            <div>
+              <h3 className="fully-int"> The same guaratees offered</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid unde, aut ipsam eos asperiores voluptate sapiente quae eius placeat autem laboriosam obcaecati maxime ex atque, cupiditate commodi saepe. Dolore.</p>
+            </div>
+          </div>
+          <div className="fully-div">
+          <FaShieldAlt className="shield"/>
+            <div>
+              <h3  className="fully-int">Amazon-discounted rates</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid unde, aut ipsam eos asperiores voluptate sapiente quae eius placeat autem laboriosam obcaecati maxime ex atque, cupiditate commodi saepe. Dolore.</p>
+            </div>
+          </div>
+          <div className="fully-div">
+          <FaShieldAlt className="shield"/>
+            <div>
+              <h3  className="fully-int"> 100% Valid Tracking Rates</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid unde, aut ipsam eos asperiores voluptate sapiente quae eius placeat autem laboriosam obcaecati maxime ex atque, cupiditate commodi saepe. Dolore.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section className="py-9">
+        <div className="my-14 flex justify-between px-[7%] ">
+          <h1 className="text-[30px] w-[48%">Get immediate discounted rates with all four major carrier </h1>
+          <p className="w-[45%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, cum perspiciatis. Quaerat vero ex, doloribus odit praesentium explicabo, quis ipsum ea pariatur quo harum assumenda recusandae. Fugiat distinctio quasi a.</p>
+       
+        </div>
+        <img src="./gastronomy-expert-cooking-succulent-beef-patty-hot-pan-while-cooking-gourmet-dish-dinner-service-restaurant-food-industry-worker-preparing-meat-meal-professional-kitchen.jpg" alt="" className="my-image"/>
+      </section>
+      <footer>
+        <div className="w-[35%]">
+          <h1>DELIVERO</h1>
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda magni nostrum soluta id, officiis officia, cumque facere possimus, illum dicta quo doloribus consequatur rem molestiae? Delectus eos debitis animi accusamus!</p>
+        </div>
+        <div className="w-[35%] flex flex-col justify-between items-center">
+          <Link href="">Home</Link>
+          <Link href="">Contact Us</Link>
+          <Link href="">About Us</Link>
+          <Link href="">Services</Link>
+        </div>
+        <div className="w-[35%] flex flex-col justify-between items-center">
+          <Link href="">Home</Link>
+          <Link href="">Contact Us</Link>
+          <Link href="">About Us</Link>
+          <Link href="">Services</Link>
+        </div>
+      </footer>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
